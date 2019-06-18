fca06d0a101cb1c0f677d23fb98a76858d5e81b3
Gavin P. Salam <gavin.salam@cern.ch>
2019-06-18 11:52:38 +0100

    fixed missing DefaultAveragingHist::_lo in AnalysisBase.cc

--- AnalysisBase.cc
+++ AnalysisBase.cc
@@ -7,6 +7,10 @@ template<> double DefaultHist::_lo =   0.0;
 template<> double DefaultHist::_hi = 100.0;
 template<> double DefaultHist::_bin_size = 1.0;
 
+template<> double DefaultHistWithError::_lo =   0.0;
+template<> double DefaultHistWithError::_hi = 100.0;
+template<> double DefaultHistWithError::_bin_size = 1.0;
+
 template<> double DefaultAveragingHist::_lo =   0.0;
 template<> double DefaultAveragingHist::_hi = 100.0;
 template<> double DefaultAveragingHist::_bin_size = 1.0;
